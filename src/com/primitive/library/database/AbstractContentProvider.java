a15bda89f8f092c8507355b4a855166563c9830d
xxx <yskkmi@gmail.com>
2013-05-13 17:59:10 +0900

    copyright adding

--- src/com/primitive/library/database/AbstractContentProvider.java
+++ src/com/primitive/library/database/AbstractContentProvider.java
@@ -1,3 +1,13 @@
+/**
+ * AbstractContentProvider
+ * 
+ * @license Dual licensed under the MIT or GPL Version 2 licenses.
+ * @author xxxzxxx
+ * Copyright 2013, Primitive, inc.
+ * The MIT Licens (http://opensource.org/licenses/mit-license.php)
+ * GPL Version 2 licenses (http://www.gnu.org/licenses/gpl-2.0.html)
+ */
+
 package com.primitive.library.database;
 
 import com.primitive.library.helper.Logger;
