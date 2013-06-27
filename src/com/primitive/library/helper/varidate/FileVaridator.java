596ce7484d2eecd7ab9f01d31e5be93456002cae
xxxzxxx <yskkmi@gmail.com>
2013-06-28 08:27:04 +0900

    varidateクラスの追加

---
+++ src/com/primitive/library/helper/varidate/FileVaridator.java
@@ -0,0 +1,138 @@
+/**
+ * FileVaridator
+ *
+ * @license Dual licensed under the MIT or GPL Version 2 licenses.
+ * @author xxxzxxx
+ * Copyright 2013, Primitive, inc.
+ * The MIT Licens (http://opensource.org/licenses/mit-license.php)
+ * GPL Version 2 licenses (http://www.gnu.org/licenses/gpl-2.0.html)
+ */
+package com.primitive.library.helper.varidate;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.NoSuchAlgorithmException;
+
+import com.primitive.library.exception.Exception;
+import com.primitive.library.exception.ObjectSettingException;
+import com.primitive.library.helper.Logger;
+import com.primitive.library.helper.cipher.MessageDigestHelper;
+import com.primitive.library.helper.cipher.MessageDigestHelper.Algorithm;
+
+public class FileVaridator {
+	/**  */
+	private Algorithm algorithm;
+	/**  */
+	private String digest;
+	/**  */
+	private InputStream inputStream;
+
+	/**
+	 *
+	 */
+	public FileVaridator() {
+	}
+
+	/**
+	 *
+	 * @param filePath
+	 * @param digest
+	 * @param algorithm
+	 * @throws FileNotFoundException
+	 */
+	public FileVaridator(final String filePath, final String digest,
+			final Algorithm algorithm) throws FileNotFoundException {
+		File file = new File(filePath);
+		this.inputStream = new FileInputStream(file);
+		this.digest = digest;
+		this.algorithm = algorithm;
+	}
+
+	/**
+	 *
+	 * @param target
+	 * @param digest
+	 * @param algorithm
+	 */
+	public FileVaridator(final InputStream inputStream, final String digest,
+			final Algorithm algorithm) {
+		this.inputStream = inputStream;
+		this.digest = digest;
+		this.algorithm = algorithm;
+	}
+
+	/**
+	 * check parameters
+	 *
+	 * @throws ObjectSettingException
+	 */
+	private void check() throws ObjectSettingException {
+		if (digest == null || inputStream == null) {
+			throw new ObjectSettingException("Parameter setup has a defect. ");
+		}
+	}
+
+	/**
+	 *
+	 *
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean compare() throws Exception {
+		check();
+		boolean result = false;
+		try {
+			int size = inputStream.available();
+			byte[] data = new byte[size];
+			if (0 != inputStream.read(data, 0, size)) {
+				String digestData = MessageDigestHelper.getDigestBase64(
+						algorithm, data);
+				result = digestData.equals(digest);
+			}
+		} catch (NoSuchAlgorithmException ex) {
+			Logger.err(ex);
+			throw new Exception(ex);
+		} catch (IOException ex) {
+			Logger.err(ex);
+			throw new Exception(ex);
+		}
+		return result;
+	}
+
+	public Algorithm getAlgorithm() {
+		return algorithm;
+	}
+
+	public void setAlgorithm(Algorithm algorithm) {
+		this.algorithm = algorithm;
+	}
+
+	public String getDigest() {
+		return digest;
+	}
+
+	public void setDigest(String digest) {
+		this.digest = digest;
+	}
+
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
+	public void setInputStream(InputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+
+	public void close() {
+		if (inputStream != null) {
+			try {
+				inputStream.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/com/primitive/library/helper/varidate/StringVaridator.java
@@ -0,0 +1,84 @@
+/**
+ * StringVaridator
+ *
+ * @license Dual licensed under the MIT or GPL Version 2 licenses.
+ * @author xxxzxxx
+ * Copyright 2013, Primitive, inc.
+ * The MIT Licens (http://opensource.org/licenses/mit-license.php)
+ * GPL Version 2 licenses (http://www.gnu.org/licenses/gpl-2.0.html)
+ */
+package com.primitive.library.helper.varidate;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
+import com.primitive.library.exception.Exception;
+import com.primitive.library.exception.ObjectSettingException;
+import com.primitive.library.helper.Logger;
+import com.primitive.library.helper.cipher.MessageDigestHelper;
+import com.primitive.library.helper.cipher.MessageDigestHelper.Algorithm;
+
+public class StringVaridator {
+	Algorithm algorithm;
+	String original;
+	String encode;
+	String digest;
+
+	/**
+	 *
+	 */
+	public StringVaridator() {
+	}
+
+	/**
+	 *
+	 * @param data
+	 * @param digest
+	 * @param encode
+	 * @param algorithm
+	 */
+	public StringVaridator(final String data, final String digest,
+			final String encode, final Algorithm algorithm) {
+		this.original = data;
+		this.digest = digest;
+		this.algorithm = algorithm;
+		this.encode = encode;
+	}
+
+	/**
+	 *
+	 * @throws ObjectSettingException
+	 */
+	private void check() throws ObjectSettingException {
+		Object[] compares = new Object[] { original, digest, algorithm, encode, };
+		for (Object obj : compares) {
+			if (obj == null) {
+				throw new ObjectSettingException(
+						"Parameter setup has a defect. ");
+			}
+		}
+	}
+
+	/**
+	 *
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean compare() throws Exception {
+		check();
+		boolean result = false;
+		try {
+			byte[] data = original.getBytes(encode);
+			String digestData = MessageDigestHelper.getDigestBase64(algorithm,
+					data);
+			result = digestData.equals(digest);
+		} catch (NoSuchAlgorithmException ex) {
+			Logger.err(ex);
+			throw new Exception(ex);
+		} catch (UnsupportedEncodingException ex) {
+			Logger.err(ex);
+			throw new Exception(ex);
+		}
+		return result;
+	}
+}
