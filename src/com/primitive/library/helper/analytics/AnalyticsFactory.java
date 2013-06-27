38feb3cec26b193a85d95c12f1e924e83e1c62a2
xxxzxxx <yskkmi@gmail.com>
2013-06-28 08:21:06 +0900

    AnalyticsFactoryでnullを返していたのを修正

--- src/com/primitive/library/helper/analytics/AnalyticsFactory.java
+++ src/com/primitive/library/helper/analytics/AnalyticsFactory.java
@@ -1,17 +1,34 @@
+/**
+ * AnalyticsFactory
+ *
+ * @license Dual licensed under the MIT or GPL Version 2 licenses.
+ * @author xxxzxxx
+ * Copyright 2013, Primitive, inc.
+ * The MIT Licens (http://opensource.org/licenses/mit-license.php)
+ * GPL Version 2 licenses (http://www.gnu.org/licenses/gpl-2.0.html)
+ */
 package com.primitive.library.helper.analytics;
 
+/**
+ *
+ * @author xxxzxxx
+ *
+ */
 public class AnalyticsFactory {
-	enum AnalyticsType{
+	enum AnalyticsType {
 		GoogleAnalytics
 	};
+
 	static GAHelper gahelper = null;
 
-	public static AnalyticsHelper createHelper(final AnalyticsType analyticsType){
-		switch (analyticsType){
+	public static AnalyticsHelper getHelper(final AnalyticsType analyticsType) {
+		AnalyticsHelper helper = null;
+		switch (analyticsType) {
 		case GoogleAnalytics:
 			gahelper = gahelper != null ? gahelper : new GAHelper();
+			helper = gahelper;
 			break;
 		}
-		return null;
+		return helper;
 	}
 }
